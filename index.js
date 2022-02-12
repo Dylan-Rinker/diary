da17a719a7be1b02de8775f8b7d85ad780f8a744
Dylan Rinker <dylan.p.rinker@gmail.com>
2022-02-12 16:34:55 -0500

    Add new delete functionality

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ async function writeFunction() {
 	};
 
 	writeEntry(entry);
-	// console.log(object);
 }
 
 function writeEntry(entry) {
@@ -97,9 +96,73 @@ async function writeLabels() {
 program.command("read")
 	.description("Read entries from your diary")
 	.option("-d, --date <date>", "date to read")
+	.option("-l, --label <label>", "label to read")
+	.option("-s, --search", "search entry to read")
+	.option("-u, --uuid <uuid>", "uuid to read")
 	.action((options) => {
-		console.log(chalk.red(`This is the read tes operation. This option is ${options.date}`));
+		console.log(`Options: ${options}`);
+
+		if (isOptionsEmpty(options)) {
+			readDiary();
+		}
+
+		if (options.search) {
+			listEntries();
+			console.log('This is the options search.')
+		}
 	});
+
+function readDiary(quantity) {
+	const db = fs.readFile("./database.json", "utf8", (err, data) => {
+		if (err) {
+			console.log(err);
+			return
+		};
+
+		if (data) {
+			let db = JSON.parse(data);
+			let entries = db.entries
+			console.log(entries)
+			console.log(entries.length)
+			console.log(entries[0])
+			console.log(entries.reverse().slice(0,10))
+			
+			return entries
+		}
+
+		console.log(chalk.yellow('No data found'))
+	});
+
+}
+
+function listEntries() {
+	const entries = returnEntries();
+	console.log(`entries: ${entries}`);
+	let formattedEntries = formatEntries(entries);
+	console.log(`formattedEntries: ${formattedEntries}`);
+	// const answers = await inquirer.prompt({
+	// 	type: "list",
+	// 	name: "entry",
+	// 	message: "Which entry do you want to read?",
+	// 	choices: formattedEntries,
+	// });
+
+	// console.log(answers);
+}
+
+function formatEntries(entries) {
+	let formattedEntries = entries.map(entry => {
+		return `${entry.title} ${entry.uuid} ${entry.created}`
+	});
+	return formattedEntries;
+}
+
+function isOptionsEmpty(options) {
+	if (Object.keys(options).length === 0) {
+		return true;
+	}
+	return false;
+}
 
 program.command("edit")
 	.description("Edit an entry to your diary")
@@ -111,30 +174,68 @@ program.command("edit")
 program.command("delete")
 	.description("Delete an entry from your diary")
 	.option("-u, --uuid <uuid>", "uuid of entry to delete")
-	.action((options) => {
-		console.log(`Delete options: ${options}`);
+	.action(async (options) => {
+		console.log('Options:', options)
 		if (Object.keys(options).length === 0) {
-			deletePrompt();
+			await deletePrompt();
 		}
 
-		console.log("This is the delete operation");
+		if (options.uuid) {
+			if (!(isUUIDValid(options.uuid))) {
+				console.log(chalk.red("The uuid is not valid."));
+			}
+			else {
+				deleteEntry(options.uuid);
+			}
+		}
 	});
 
-function deletePrompt() {
+function isUUIDValid(uuid) {
+	if (!(uuid.length === 12 || uuid.length === 36)) {
+		return false
+	}
+	
+	// ToDo: Maybe is UUID in list of UUIDs?
+	return true;
+}
+
+function deleteEntry(uuid) {
+	const db = fs.readFileSync("./database.json", "utf8");
+	let newDB = JSON.parse(db);
+	
+	switch (uuid.length) {
+		case 12:
+			newDB.entries = newDB.entries.filter(entry => {
+				let miniUUID = entry.uuid.split('-')[4]
+				return miniUUID !== uuid;
+			});
+			break;
+		case 36:
+			console.log('the uuid is 36')
+			newDB.entries = newDB.entries.filter(entry => {
+				return entry.uuid !== uuid;
+			});
+			break;
+		default:
+			console.log('The UUID length is not valid.')
+	}
+
+	fs.writeFileSync("./database.json", JSON.stringify(newDB));
+}
+
+async function deletePrompt() {
 	const entries = returnEntries();
-	console.log(entries)
+	console.log(`entries: ${entries}`);
+	let formattedEntries = formatEntries(entries);
 
 	inquirer.prompt({
 		type: "list",
 		name: "delete",
 		message: "Which entries do you want to delete?",
-		choices: [
-			"Entry",
-			"Label",
-			"Both",
-		],
+		choices: formattedEntries,
 	}).then(answers => {
 		console.log(answers);
+
 		if (answers.delete === "Entry") {
 			deleteEntry();
 		}
@@ -149,8 +250,11 @@ function deletePrompt() {
 }
 
 function returnEntries() {
-	const entries = fs.readFileSync("./database.json", "utf8");
-	return JSON.parse(entries);
+	let db = fs.readFileSync("./database.json", "utf8")
+	let data = JSON.parse(db);
+	let entries = data.entries
+	console.log(`entries: ${entries}`);
+	return entries;
 }
 
 program.command("search")
@@ -175,10 +279,11 @@ program.command("config")
 	.action((options) => {
 		console.log(options)
 		if (options.get) {
-			fs.readFile("./config.json", 'utf8', (err, data) => {
+			fs.readFileSync("./config.json", 'utf8', (err, data) => {
 				if (err) {
 					return console.log(err);
 				}
+
 				if (data) {
 					const config = JSON.parse(data);
 					if (!(config[options.get])) {
@@ -194,7 +299,7 @@ program.command("config")
 			// ToDo: This will have to read from the globally installed config file after npm install -g.
 		}
 		if (options.getAll) {
-			fs.readFile("./config.json", 'utf8', (err, data) => {
+			fs.readFileSync("./config.json", 'utf8', (err, data) => {
 				if (err) {
 					return console.log(err);
 				}
@@ -220,8 +325,3 @@ program.command("config")
 			);
 
 program.parse(process.argv);
-
-// A function that check if a key exists in the config file.
-function checkIfKeyExists() {
-
-}
